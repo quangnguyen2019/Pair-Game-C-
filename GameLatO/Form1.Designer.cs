8aef50cbd24a16f3acf7eff4b00e12aed612fa37
Quang <nmquang2012@gmail.com>
2020-09-25 11:06:36 +0700

    Pair Game Project C#

---
+++ GameLatO/Form1.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace GameLatO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timerForCells = new System.Windows.Forms.Timer(this.components);
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.numcUpDownColumn = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numUpDownRow = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.lbNotification = new System.Windows.Forms.Label();
+            this.lbTimeRemaining = new System.Windows.Forms.Label();
+            this.timerCountdown = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.panelCountdown = new System.Windows.Forms.Panel();
+            this.cbLevels = new System.Windows.Forms.ComboBox();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numcUpDownColumn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numUpDownRow)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.panelCountdown.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timerForCells
+            // 
+            this.timerForCells.Interval = 300;
+            this.timerForCells.Tick += new System.EventHandler(this.timerForCells_Tick);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "003-Venusaur-icon.png");
+            this.imageList1.Images.SetKeyName(1, "006-Charizard-icon.png");
+            this.imageList1.Images.SetKeyName(2, "009-Blastoise-icon.png");
+            this.imageList1.Images.SetKeyName(3, "012-Butterfree-icon.png");
+            this.imageList1.Images.SetKeyName(4, "015-Beedrill-icon.png");
+            this.imageList1.Images.SetKeyName(5, "018-Pidgeot-icon.png");
+            this.imageList1.Images.SetKeyName(6, "020-Raticate-icon.png");
+            this.imageList1.Images.SetKeyName(7, "021-Spearow-icon.png");
+            this.imageList1.Images.SetKeyName(8, "022-Fearow-icon.png");
+            this.imageList1.Images.SetKeyName(9, "025-Pikachu-icon.png");
+            this.imageList1.Images.SetKeyName(10, "026-Raichu-icon.png");
+            this.imageList1.Images.SetKeyName(11, "037-Vulpix-icon.png");
+            this.imageList1.Images.SetKeyName(12, "038-Ninetales-icon.png");
+            this.imageList1.Images.SetKeyName(13, "059-Arcanine-icon.png");
+            this.imageList1.Images.SetKeyName(14, "069-Bellsprout-icon.png");
+            this.imageList1.Images.SetKeyName(15, "078-Rapidash-icon.png");
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.cbLevels);
+            this.panel2.Controls.Add(this.btnCreate);
+            this.panel2.Controls.Add(this.numcUpDownColumn);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.numUpDownRow);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(12, 26);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 3, 3, 10);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(594, 41);
+            this.panel2.TabIndex = 4;
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCreate.Location = new System.Drawing.Point(483, 3);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(104, 36);
+            this.btnCreate.TabIndex = 4;
+            this.btnCreate.Text = "Start";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // numcUpDownColumn
+            // 
+            this.numcUpDownColumn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numcUpDownColumn.Location = new System.Drawing.Point(145, 11);
+            this.numcUpDownColumn.Maximum = new decimal(new int[] {
+            6,
+            0,
+            0,
+            0});
+            this.numcUpDownColumn.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numcUpDownColumn.Name = "numcUpDownColumn";
+            this.numcUpDownColumn.Size = new System.Drawing.Size(63, 22);
+            this.numcUpDownColumn.TabIndex = 3;
+            this.numcUpDownColumn.Value = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(214, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Columns";
+            // 
+            // numUpDownRow
+            // 
+            this.numUpDownRow.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numUpDownRow.Location = new System.Drawing.Point(12, 11);
+            this.numUpDownRow.Maximum = new decimal(new int[] {
+            6,
+            0,
+            0,
+            0});
+            this.numUpDownRow.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numUpDownRow.Name = "numUpDownRow";
+            this.numUpDownRow.Size = new System.Drawing.Size(63, 22);
+            this.numUpDownRow.TabIndex = 1;
+            this.numUpDownRow.Value = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(82, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Rows";
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSize = true;
+            this.panel1.Location = new System.Drawing.Point(21, 14);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(103, 62);
+            this.panel1.TabIndex = 5;
+            // 
+            // panel3
+            // 
+            this.panel3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel3.Controls.Add(this.panel1);
+            this.panel3.Location = new System.Drawing.Point(24, 145);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(575, 362);
+            this.panel3.TabIndex = 6;
+            // 
+            // lbNotification
+            // 
+            this.lbNotification.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lbNotification.AutoSize = true;
+            this.lbNotification.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbNotification.ForeColor = System.Drawing.Color.Red;
+            this.lbNotification.Location = new System.Drawing.Point(259, 525);
+            this.lbNotification.Margin = new System.Windows.Forms.Padding(3, 0, 3, 10);
+            this.lbNotification.Name = "lbNotification";
+            this.lbNotification.Size = new System.Drawing.Size(136, 25);
+            this.lbNotification.TabIndex = 8;
+            this.lbNotification.Text = "lbNotification";
+            // 
+            // lbTimeRemaining
+            // 
+            this.lbTimeRemaining.AutoSize = true;
+            this.lbTimeRemaining.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTimeRemaining.Location = new System.Drawing.Point(34, 0);
+            this.lbTimeRemaining.Name = "lbTimeRemaining";
+            this.lbTimeRemaining.Size = new System.Drawing.Size(117, 42);
+            this.lbTimeRemaining.TabIndex = 7;
+            this.lbTimeRemaining.Text = "00:00";
+            // 
+            // timerCountdown
+            // 
+            this.timerCountdown.Interval = 1000;
+            this.timerCountdown.Tick += new System.EventHandler(this.timerCountdown_Tick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Image = ((System.Drawing.Image)(resources.GetObject("label4.Image")));
+            this.label4.Location = new System.Drawing.Point(3, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(28, 42);
+            this.label4.TabIndex = 9;
+            this.label4.Text = " ";
+            // 
+            // panelCountdown
+            // 
+            this.panelCountdown.Controls.Add(this.lbTimeRemaining);
+            this.panelCountdown.Controls.Add(this.label4);
+            this.panelCountdown.Location = new System.Drawing.Point(236, 87);
+            this.panelCountdown.Name = "panelCountdown";
+            this.panelCountdown.Size = new System.Drawing.Size(155, 42);
+            this.panelCountdown.TabIndex = 10;
+            // 
+            // cbLevels
+            // 
+            this.cbLevels.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbLevels.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbLevels.FormattingEnabled = true;
+            this.cbLevels.Location = new System.Drawing.Point(298, 11);
+            this.cbLevels.Name = "cbLevels";
+            this.cbLevels.Size = new System.Drawing.Size(160, 24);
+            this.cbLevels.TabIndex = 5;
+            this.cbLevels.SelectedIndexChanged += new System.EventHandler(this.cbLevels_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(621, 578);
+            this.Controls.Add(this.panelCountdown);
+            this.Controls.Add(this.lbNotification);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pairs Game";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numcUpDownColumn)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numUpDownRow)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panelCountdown.ResumeLayout(false);
+            this.panelCountdown.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timerForCells;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.NumericUpDown numcUpDownColumn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numUpDownRow;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label lbTimeRemaining;
+        private System.Windows.Forms.Timer timerCountdown;
+        private System.Windows.Forms.Label lbNotification;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panelCountdown;
+        private System.Windows.Forms.ComboBox cbLevels;
+    }
+}
+
---
+++ GameLatO/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace GameLatO
+{
+    public partial class Form1 : Form
+    {
+        double time;
+        DateTime start;
+        DateTime endTime;
+        TimeSpan remainingTime;
+        int remainPairs;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            var levels = new[]
+            {
+                new { Text = "Easy ( 2 minutes )", Value = 2.0},
+                new { Text = "Medium ( 1,5 minutes )", Value = 1.5},
+                new { Text = "Hard ( 1 minutes )", Value = 1.0}
+            };
+
+            cbLevels.DisplayMember = "Text";
+            cbLevels.ValueMember = "Value";
+            cbLevels.DataSource = levels;
+            time = (double)cbLevels.SelectedValue;
+
+            lbNotification.Text = "";
+            panelCountdown.Location = new Point(
+                (Width - panelCountdown.Width) / 2,
+                panelCountdown.Location.Y
+            );
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            panel1.Controls.Clear();
+            panel1.Enabled = true;
+            lbNotification.Text = "";
+
+            int row = (int)numUpDownRow.Value;
+            int column = (int)numcUpDownColumn.Value;
+
+            if (row * column % 2 != 0)
+            {
+                timerCountdown.Enabled = false;
+                string str = "The total number of cells ( Col x Row ) must be an EVEN NUMBER !!";
+                MessageBox.Show(str, "Notification");
+                return;
+            }
+
+            // reset thời gian đếm ngược
+            start = DateTime.UtcNow;
+            endTime = start.AddMinutes(time);
+            remainingTime = endTime - DateTime.UtcNow;
+            lbTimeRemaining.Text = remainingTime.ToString(@"mm\:ss");
+            timerCountdown.Enabled = true;
+
+            // Số cặp còn lại sau khi mở
+            remainPairs = (row * column) / 2;
+
+            int btnSize = CalculateButtonSize(row, column);
+            // List chứa các button
+            List<CustomButton> listButtons = GenerateButtons(row, column, btnSize);
+            PutButtonsToGrid(row, column, btnSize, listButtons);
+
+            // Căn chỉnh thời gian chính giữa
+            panelCountdown.Location = new Point(
+                (Width - panelCountdown.Width) / 2,
+                panelCountdown.Location.Y
+            );
+        }
+
+        CustomButton btnOpenedFirst = null;
+        CustomButton btnOpenedSecond = null;
+
+        private void BtnCell_Click(object sender, EventArgs e)
+        {
+            CustomButton btn = (CustomButton)sender;
+            btn.BackgroundImage = (Image)btn.Tag;
+
+            if (btnOpenedFirst == null)
+            {
+                btnOpenedFirst = btn;
+            }
+            else
+            {
+                // Kiểm tra có click vào cùng 1 button không?
+                if (btnOpenedFirst.Name != btn.Name)
+                {
+                    btnOpenedSecond = btn;
+                    timerForCells.Enabled = true;
+                }
+            }
+        }
+
+        private void timerForCells_Tick(object sender, EventArgs e)
+        {
+            if (btnOpenedFirst.CoupleIndex != btnOpenedSecond.CoupleIndex)
+            {
+                btnOpenedFirst.BackgroundImage = null;
+                btnOpenedSecond.BackgroundImage = null;
+            }
+            else
+            {
+                //panel1.Controls.Remove(btnOpenedFirst);
+                //panel1.Controls.Remove(btnOpenedSecond);
+                btnOpenedFirst.Enabled = false;
+                btnOpenedSecond.Enabled = false;
+                remainPairs--;
+            }
+
+            btnOpenedFirst = null;
+            timerForCells.Enabled = false;
+        }
+
+        public int random(int val)
+        {
+            return (new Random()).Next(val);
+        }
+
+        private int CalculateButtonSize(int row, int column)
+        {
+            int btnSizeByRow = (int)Math.Floor((double)(panel3.Height - (row - 1) * 5) / row);
+            int btnSizeByColumn = (int)Math.Floor((double)(panel3.Width - (column - 1) * 5) / column);
+            return Math.Min(btnSizeByRow, btnSizeByColumn);
+        }
+
+        private List<CustomButton> GenerateButtons(int row, int column, int btnSize)
+        {
+            // List chứa các button
+            List<CustomButton> listButtons = new List<CustomButton>();
+
+            // Thêm 1 cặp button giống nhau vào listButtons
+            for (int i = 0; i < (row * column / 2); i++)
+            {
+                CustomButton btn1 = new CustomButton();
+                btn1.Name = (i * 2 - 1).ToString();
+                btn1.Size = new Size(btnSize, btnSize);
+                btn1.Tag = imageList1.Images[i % 16];
+                btn1.BackgroundImageLayout = ImageLayout.Stretch;
+                btn1.CoupleIndex = i;
+                btn1.Click += BtnCell_Click;
+                listButtons.Add(btn1);
+
+                CustomButton btn2 = new CustomButton();
+                btn2.Name = (i * 2).ToString();
+                btn2.Size = new Size(btnSize, btnSize);
+                btn2.Tag = imageList1.Images[i % 16];
+                btn2.BackgroundImageLayout = ImageLayout.Stretch;
+                btn2.CoupleIndex = i;
+                btn2.Click += BtnCell_Click;
+                listButtons.Add(btn2);
+            }
+
+            return listButtons;
+        }
+
+        private void PutButtonsToGrid(int row, int column, int btnSize, List<CustomButton> listButtons)
+        {
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    int index = random(listButtons.Count);
+                    listButtons[index].Location = new Point((btnSize + 5) * j, (btnSize + 5) * i);
+                    panel1.Controls.Add(listButtons[index]);
+                    listButtons.RemoveAt(index);
+                }
+            }
+
+            // Căn chỉnh các ô nằm chính giữa
+            int sizeTemp = listButtons.Count * (btnSize + 5);
+            panel1.Size = new Size(sizeTemp, sizeTemp);
+            panel1.Location = new Point(
+                (panel3.Width - panel1.Width) / 2,
+                (panel3.Height - panel1.Height) / 2
+            );
+        }
+
+        private void timerCountdown_Tick(object sender, EventArgs e)
+        {
+            // Cập nhật thời gian còn lại (được hiển thị)
+            remainingTime = endTime - DateTime.UtcNow;
+            lbTimeRemaining.Text = remainingTime.ToString(@"mm\:ss");
+
+            // Số cặp còn lại = 0
+            if (remainPairs == 0)
+            {
+                timerCountdown.Enabled = false;
+                //MessageBox.Show("You Win!!");
+                lbNotification.Text = "You Win!!";
+            }
+            else if (remainingTime < TimeSpan.Zero)
+            {
+                timerCountdown.Enabled = false;
+                panel1.Enabled = false;
+                //MessageBox.Show("Game Over!!");
+                lbNotification.Text = "Game Over!!";
+            }
+        }
+
+        private void cbLevels_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            time = (double)cbLevels.SelectedValue;
+            timerCountdown.Enabled = false;
+            lbTimeRemaining.Text = "00:00";
+            panel1.Controls.Clear();
+        }
+    }
+}
